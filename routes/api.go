f6369656d4f673655df8ad00e0b54419b1085efe
Jomari <cpe.ecaljomari@gmail.com>
2024-12-07 16:18:41 +0800

    Dynamic Routes has added

--- routes/api.go
+++ routes/api.go
@@ -11,4 +11,11 @@ func RegisterRoutes(app *fiber.App) {
 	// Example endpoints
 	api.Get("/info", handlers.GetInfo)       // Fetch static info
 	api.Post("/submit", handlers.SubmitData) // Handle submitted data
+
+	app.Get("/users/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("User ID: " + id)
+	})
+	
 }
+
